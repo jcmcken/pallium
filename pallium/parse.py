b214c91e6ce6faa73814f0e27e59b96d0420e8cd
Jon McKenzie <jcmcken@gmail.com>
2012-07-28 00:32:24 -0400

    Made GangliaContentHandler more modular for subclassability

--- pallium/parse.py
+++ pallium/parse.py
@@ -2,7 +2,12 @@ import xml.sax as sax
 import re
 
 class GangliaContentHandler(sax.ContentHandler):
-    def __init__(self, datastore, grid_expr=".*", cluster_expr=".*", host_expr=".*"):
+    """
+    Base Ganglia content handler. Does not do anything with the data, but it will
+    execute logic to filter out data that doesn't match ``grid_expr``, 
+    ``cluster_expr``, or ``host_expr``. 
+    """
+    def __init__(self, grid_expr=".*", cluster_expr=".*", host_expr=".*"):
 
         self.exprs = {
             'grid': re.compile(grid_expr),
@@ -16,8 +21,6 @@ class GangliaContentHandler(sax.ContentHandler):
 
         self.skip = {}
 
-        self.data = datastore
-
         sax.ContentHandler.__init__(self)
 
     def _eval_re(self, type, text):
@@ -27,9 +30,6 @@ class GangliaContentHandler(sax.ContentHandler):
             raise ValueError("Invalid regular expression type '%s" % type)
 
         return bool(pat.search(text))
-
-    def startDocument(self):
-        print 'started xml parsing'
 
     def _check_skip(self, type):
         skip = self.skip.get(type, None)
@@ -55,7 +55,10 @@ class GangliaContentHandler(sax.ContentHandler):
         # if grid doesn't match regexp, set skip
         if self._set_skip('grid', name): return
 
-        self.data.setdefault(name, {})
+        self.handle_grid(attrs)
+
+    def handle_grid(self, attrs):
+        self.data.setdefault(attrs['NAME'], {})
 
     def _handle_cluster(self, attrs):
         name = attrs['NAME']
@@ -68,7 +71,10 @@ class GangliaContentHandler(sax.ContentHandler):
         # skip node if GRID set to skip
         if self._check_skip('grid'): return
 
-        self.data[self.current['grid']].setdefault(name, {})
+        self.handle_cluster(attrs)
+
+    def handle_cluster(self, attrs):
+        self.data[self.current['grid']].setdefault(attrs['NAME'], {})
 
     def _handle_host(self, attrs):
         name = attrs['NAME']
@@ -80,8 +86,11 @@ class GangliaContentHandler(sax.ContentHandler):
         # skip node if GRID or CLUSTER set to skip
         if self._check_skip('grid'): return 
         if self._check_skip('cluster'): return
-        
-        self.data[self.current['grid']][self.current['cluster']].setdefault(name, {})
+
+        self.handle_host(attrs)
+
+    def handle_host(self, attrs):
+        self.data[self.current['grid']][self.current['cluster']].setdefault(attrs['NAME'], {})
 
     def _handle_metric(self, attrs):
         name = attrs['NAME']
@@ -90,6 +99,10 @@ class GangliaContentHandler(sax.ContentHandler):
         if self._check_skip('grid'): return 
         if self._check_skip('cluster'): return
         if self._check_skip('host'): return
+
+        self.handle_metric(attrs)
+
+    def handle_metric(self, attrs):
 
         metric_data = { 
             'name': attrs['VAL'], 
@@ -103,16 +116,31 @@ class GangliaContentHandler(sax.ContentHandler):
     def _handle_grid_end(self):
         self.current['grid'] = None
         self.skip['grid'] = None
+        self.handle_grid_end()
+
+    def handle_grid_end(self):
+        pass
 
     def _handle_cluster_end(self):
         self.current['cluster'] = None
         self.skip['cluster'] = None
+        self.handle_cluster_end()
+
+    def handle_cluster_end(self):
+        pass
 
     def _handle_host_end(self):
         self.current['host'] = None
         self.skip['host'] = None
+        self.handle_host_end()
+
+    def handle_host_end(self):
+        pass
 
     def _handle_metric_end(self):
+        self.handle_metric_end()
+
+    def handle_metric_end(self):
         pass
 
     def startElement(self, name, attrs):
@@ -140,3 +168,47 @@ class GangliaContentHandler(sax.ContentHandler):
             self._handle_metric_end()
 
         else: return # ignore everything else
+
+class LoadingGangliaContentHandler(GangliaContentHandler):
+    """
+    A Ganglia content handler that simply loads the Gmetad data structure into
+    a dictionary.
+
+    Without specifying grid/cluster/host filter regexps, the whole structure 
+    will be loaded into ``data``.
+
+    Structure is of the form::
+
+        {"grid_name": 
+          "cluster_name": {
+            "host_name": {
+              "metric_name_1": { "value": "...", "units": "...", "type": "..." },
+              "metric_name_2": { "value": "...", "units": "...", "type": "..." },
+              ...
+            }
+          }
+        }
+    """
+    def __init__(self, **kwargs):
+        self.data = {}
+        GangliaContentHandler.__init__(self, **kwargs)
+    
+    def handle_grid(self, attrs):
+        self.data.setdefault(attrs['NAME'], {})
+    
+    def handle_cluster(self, attrs):
+        self.data[self.current['grid']].setdefault(attrs['NAME'], {})
+    
+    def handle_host(self, attrs):
+        self.data[self.current['grid']][self.current['cluster']].setdefault(attrs['NAME'], {})
+    
+    def handle_metric(self, attrs):
+
+        metric_data = { 
+            'value': attrs['VAL'], 
+            'units': attrs['UNITS'], 
+            'type': attrs['TYPE']
+        } 
+
+        self.data[self.current['grid']][self.current['cluster']]\
+                 [self.current['host']][attrs['NAME']] = metric_data
