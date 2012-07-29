7d0dd02019e7ec27f554402a8513fe85924a674d
Jon McKenzie <jcmcken@gmail.com>
2012-07-28 21:29:54 -0400

    Function to validate simple tag names

--- pallium/config.py
+++ pallium/config.py
@@ -4,7 +4,11 @@ try:
 except ImportError:
     import simplejson as json
 
+import re
 from copy import deepcopy
+
+_STR_RE_VALID_TAG = "^[A-Za-z0-9_\-]+$"
+_RE_VALID_TAG = re.compile(_STR_RE_VALID_TAG)
 
 DEFAULT_CONFIG_FILE = "/etc/pallium/config.json"
 
@@ -21,3 +25,5 @@ def load_pallium_config(filename):
 def load_json_config(filename):
     return json.load(open(filename))
 
+def valid_tag(tag):
+    return bool(_RE_VALID_TAG.search(tag))
