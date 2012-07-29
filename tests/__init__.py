8caa3e0e284b9fbd1fba15d687afd89ee1d09e12
Jon McKenzie <jcmcken@gmail.com>
2012-07-29 17:07:28 -0400

    Added more test config values

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,6 @@ import os
 TEST_ROOT = os.path.dirname(os.path.abspath(__file__))
 FIXTURES_DIR = os.path.join(TEST_ROOT, 'fixtures')
 ALERT_DIR = os.path.join(FIXTURES_DIR, 'alerts')
+FAKE_ALERTS_DIR = os.path.join(ALERT_DIR, 'fake')
+REAL_ALERTS_DIR = os.path.join(ALERT_DIR, 'real')
 XML_DIR = os.path.join(FIXTURES_DIR, 'xml')
